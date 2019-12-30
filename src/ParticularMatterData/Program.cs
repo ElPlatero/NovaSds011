27d02d7f3cb048687db067203e29ba1df31c1602
ElPlatero <elplatero@baelgun.de>
2019-12-30 14:01:57 +0100

    initial commit

---
+++ src/ParticularMatterData/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace Codehaufen.Sds011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            Console.WriteLine("The following serial ports were found:");
+            foreach (string port in ports)
+            {
+                Console.WriteLine(port);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
