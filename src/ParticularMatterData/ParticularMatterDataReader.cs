17a95a4cc2751eca252aa992359ec01fd06e0911
ElPlatero <elplatero@baelgun.de>
2019-12-31 14:09:22 +0100

    reorganized flow

---
+++ src/ParticularMatterData/ParticularMatterDataReader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Codehaufen.Sds011
+{
+    class ParticularMatterDataReader
+    {
+        private readonly Stream _baseStream;
+
+        public ParticularMatterDataReader(Stream baseStream)
+        {
+            _baseStream = baseStream;
+        }
+
+        public IEnumerable<ParticularMatterDataPacket> ReadPackets(int bytesToRead)
+        {
+            var buffer = new byte[bytesToRead];
+            _baseStream.Read(buffer, 0, buffer.Length);
+            for(int index = 0; index < buffer.Length; index++)
+            {
+                if (buffer[index] != ParticularMatterDataPacket.MessageHeader) continue;
+                if (buffer[++index] != ParticularMatterDataPacket.CommanderNo) continue;
+
+                index++;
+                var result = new ParticularMatterDataPacket();
+                result.SetPm25(ReadUint(buffer, ref index));
+                result.SetPm10(ReadUint(buffer, ref index));
+
+                result.Id1 = buffer[index++];
+                result.Id2 = buffer[index++];
+
+                if (buffer[index++] != result.Checksum) continue;
+                if (buffer[index] != ParticularMatterDataPacket.MessageTail) continue;
+                yield return result;
+            }
+        }
+
+        private uint ReadUint(byte[] buffer, ref int index)
+        {
+            var lowByte = buffer[index++];
+            var hiByte = buffer[index++];
+            return (uint)((hiByte << 8) | lowByte);
+        }
+    }
+}
---
+++ src/ParticularMatterData/ParticularMatterSensor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO.Ports;
+using System.Linq;
+
+namespace Codehaufen.Sds011
+{
+    class ParticularMatterSensor : IDisposable
+    {
+        private readonly SerialPort _port;
+
+        public ParticularMatterSensor(string portName)
+        {
+            _port = new SerialPort(portName, 9600, Parity.None, 8);
+            _port.DataReceived += PortOnDataReceived;
+        }
+
+        public void BeginReceive() => _port.Open();
+        public void Dispose() => _port?.Dispose();
+
+        private void PortOnDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (e.EventType == SerialData.Chars && sender is SerialPort serialPort)
+            {
+                var bufferSize = serialPort.BytesToRead;
+                var reader = new ParticularMatterDataReader(serialPort.BaseStream);
+                var resultList = reader.ReadPackets(bufferSize).ToList();
+                resultList.ForEach(OnPacketReceived);
+            }
+        }
+
+        public event EventHandler<ParticularMatterDataPacket> PacketReceived;
+        private void OnPacketReceived(ParticularMatterDataPacket packet) => PacketReceived?.Invoke(this, packet);
+
+    }
+}
